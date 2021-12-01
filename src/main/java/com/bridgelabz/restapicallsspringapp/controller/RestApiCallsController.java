73022e0752f775550df01d118fc2e6a34cdee04c
SRC0747 <RjSrc@0747>
2021-12-01 22:03:35 +0530

    Added : Code to invoke query parameters getting greeting message.

--- src/main/java/com/bridgelabz/restapicallsspringapp/controller/RestApiCallsController.java
+++ src/main/java/com/bridgelabz/restapicallsspringapp/controller/RestApiCallsController.java
@@ -1,6 +1,7 @@
 package com.bridgelabz.restapicallsspringapp.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,5 +12,10 @@ public class RestApiCallsController {
         return "Hello From BridgeLabz";
     }
 
-    
+    @GetMapping(value = "/message1")
+    public String getMessageInvokingQueryParameter
+            (@RequestParam String firstName,
+             @RequestParam String lastName) {
+        return "Hello From BridgeLabz : " + firstName + " " + lastName;
+    }
 }
---
+++ src/main/java/com/bridgelabz/restapicallsspringapp/dto/UserDTO.java
@@ -0,0 +1,30 @@
+package com.bridgelabz.restapicallsspringapp.dto;
+
+public class UserDTO {
+    private String firstName;
+    private String lastName;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Override
+    public String toString() {
+        return "UserDto{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
+    }
+}
