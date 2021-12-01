e5ed742d297355af540141014bd95ff1eb550b73
SRC0747 <RjSrc@0747>
2021-12-01 21:26:44 +0530

    Added : Welcome to SpringApp of REST API calls to print welcome meesage.

---
+++ src/main/java/com/bridgelabz/restapicallsspringapp/RestApiCallsSpringappApplication.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.restapicallsspringapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiCallsSpringappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiCallsSpringappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bridgelabz/restapicallsspringapp/RestApiCallsSpringappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.restapicallsspringapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiCallsSpringappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
