d3c5880e26e4b0f77baecac267c4ed9152389816
SRC0747 <RjSrc@0747>
2021-12-01 22:10:11 +0530

    Added : Code to invoke path variable getting greeting message.

--- src/main/java/com/bridgelabz/restapicallsspringapp/controller/RestApiCallsController.java
+++ src/main/java/com/bridgelabz/restapicallsspringapp/controller/RestApiCallsController.java
@@ -1,6 +1,7 @@
 package com.bridgelabz.restapicallsspringapp.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,5 +18,11 @@ public class RestApiCallsController {
             (@RequestParam String firstName,
              @RequestParam String lastName) {
         return "Hello From BridgeLabz : " + firstName + " " + lastName;
+    }
+
+    @GetMapping(value = "/message2/{name}")
+    public String getMessageInvokingPathVariable
+            (@PathVariable String name) {
+        return "Hello : " + name + " from BridgeLabz";
     }
 }
